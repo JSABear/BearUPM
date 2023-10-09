6a71ec692aadbd97b16f7a31942f4e5b0853e0e5
JSABear <111732573+JSABear@users.noreply.github.com>
2023-10-09 20:59:41 +0300

    Update RotateCube.cs

--- RotateCube.cs
+++ RotateCube.cs
@@ -1,15 +1,21 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class RotateCube : MonoBehaviour
+public class CubeRotation : MonoBehaviour
 {
-    public float rotationSpeed = 50f;
+    [SerializeField]
+    private float rotationSpeed = 50f; // Serialize the field for inspection in the Unity Editor
 
-    void Update()
+    private void Update()
     {
         // Rotate the GameObject (cube) around its Y-axis
-        transform.Rotate(Vector3.up * rotationSpeed * Time.deltaTime);
+        RotateCube();
+    }
+
+    private void RotateCube()
+    {
+        // Rotate the cube around its Y-axis with a smooth rotation
+        float rotationAmount = rotationSpeed * Time.deltaTime;
+        transform.Rotate(Vector3.up * rotationAmount);
     }
 }
 
