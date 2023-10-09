2898684d8e34702d1ab24df324982759cd96e055
JSABear <111732573+JSABear@users.noreply.github.com>
2023-10-09 20:56:33 +0300

    Update RotateCube.cs

--- RotateCube.cs
+++ RotateCube.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CubeRotation : MonoBehaviour
+public class RotateCube : MonoBehaviour
 {
     public float rotationSpeed = 50f;
 
